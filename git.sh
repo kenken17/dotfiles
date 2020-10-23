c1840371a3668359a2d80d2a30fa0c0984b5575a
ken <kenken17@gmail.com>
2020-10-23 09:41:10 +0800

    Make log more info for oneliner

--- git.sh
+++ git.sh
@@ -21,7 +21,7 @@ git config --global alias.sa "stash apply"
 git config --global alias.la "!git config -l | grep alias | cut -c 7-"
 git config --global alias.ll 'log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cgreen\\ [%cn]" --decorate --numstat'
 git config --global alias.ls 'log --pretty=format:"%C(yellow)%h\\ %ad%Cred%d\\ %Creset%s%Cgreen\\ [%cn]" --decorate --date=relative'
-git config --global alias.l "log --graph --oneline --decorate --all"
+git config --global alias.l 'log --graph --oneline --decorate --all --date=relative --pretty=format:"%C(yellow)%h%C(reset)%C(auto)%d %C(reset)%s - %C(green)[%cn] %C(dim white)%cd"'
 git config --global alias.lb "log --graph --oneline --decorate"
 git config --global alias.dl "!git ll -1"
 git config --global alias.dlc "diff --cached HEAD^"
