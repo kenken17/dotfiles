3910d3e5c3eeac6dfbd4a05bc68d1bfd37e0b73f
ken <kenken17@gmail.com>
2021-01-05 22:18:44 +0800

    Add in alias for parrot

--- parrot-link.sh
+++ parrot-link.sh
@@ -10,6 +10,7 @@ then
   echo "# Personalise bash profile" >> ~/.bashrc
   echo "export DOTFILE_INIT=1" >> ~/.bashrc
   echo "source ~/dotfiles/parrot/.bash_profile" >> ~/.bashrc
+  echo "source ~/dotfiles/parrot/.alias" >> ~/.bashrc
 
   echo "Patch .bashrc done. Please restart session."
 else
